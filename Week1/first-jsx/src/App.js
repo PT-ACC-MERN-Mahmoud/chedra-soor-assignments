69236033d65312409430c8ac9f0531ef55f7b79f
Chedra Soor <csoor96@gmail.com>
2022-06-12 12:22:25 -0700

    First JSX Done

---
+++ Week1/first-jsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <>
+          <h1 class = "className">Hello Dojo!</h1>
+          <h2>Things I need to do:</h2>
+          <li>
+            <p>Learn React!</p>
+            <p>Play Valorant for 2hrs day</p>
+            <p>Start Practicing my Japanese</p>
+            <p>Stop procrastinating</p>
+          </li>  
+      </>
+  );
+}
+
+
+
+export default App;
