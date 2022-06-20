e4bf35b81da0569b6b525c11a58bfb0495986ea9
Chedra Soor <csoor96@gmail.com>
2022-06-19 21:03:07 -0700

    Forms

---
+++ Week2/more-forms/src/components/UserForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from  'react';
+    
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    
+    const createUser = (e) => {
+        // we must prevent the default refresh of the browser to keep our state from being reset
+        e.preventDefault();
+    
+        // create a javascript object to hold all of the values
+        const newUser = { 
+            firstName: firstName,
+            lastName:lastName,
+            email: email, 
+            password: password 
+        };
+        console.log("Welcome", newUser);
+    };
+    
+    return(
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+    );
+};
+    
+export default UserForm;
